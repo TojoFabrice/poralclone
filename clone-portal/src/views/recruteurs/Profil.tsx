51907a42ad0048e1e3ca8bdc56e5fe80432acb0a
“Natacha” <aina2natacha@gmail.com>
2023-08-18 17:22:50 +0300

    crud profil

--- clone-portal/src/views/recruteurs/Profil.tsx
+++ clone-portal/src/views/recruteurs/Profil.tsx
@@ -5,20 +5,177 @@ import { ToastContainer } from "react-toastify"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLinkedin, faSquareFacebook, faSquareTwitter } from "@fortawesome/free-brands-svg-icons";
 import profil from "../../assets/images/profil.jpg"
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import { useDispatch, useSelector } from "react-redux";
+import { getInfosRecruteur, updateRecruteur } from "../../redux/actions/Recruteur";
 
+interface IReseaux {
+    facebook: string;
+    linkedin: string;
+    twitter: string
+}
+interface IFormRecruteur {
+    user_name: string,
+    user_email: string,
+    user_role: string,
+    site_web: string,
+    siege_social: string,
+    effectif: string,
+    secteur: string,
+    apropos: string,
+    reseaux_sociaux: IReseaux
+}
 
 const Profil = () => {
+    const { token, users } = useSelector(({auth}: any) => auth)
+    const { recruteur_infos } = useSelector(({recruteur}: any) => recruteur)
+    const dispatch = useDispatch()
+
     const [image, setImage] = useState<Blob>()
     const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const [reseauxList, setReseauxList] = useState<IReseaux>({
+        facebook: "",
+        linkedin: "",
+        twitter: "",
+    })
+
+    const [formRecruteur, setFormRecruteur] = useState<IFormRecruteur>({
+        user_name: "",
+        user_email: "",
+        user_role: "",
+        site_web: "",
+        siege_social: "",
+        effectif: "",
+        secteur: "",
+        apropos: "",
+        reseaux_sociaux: reseauxList
+    })
+
+    const formFieldsRecruteur = {
+        user_name: yup
+            .string()
+            .required('Champ obligatoire'),
+        user_email: yup
+            .string()
+            .email("Email invalide")
+            .required('Champ obligatoire'),
+        site_web: yup
+            .string(),
+        siege_social: yup
+            .string(),
+        effectif: yup
+            .string(),
+        secteur: yup
+            .string(),
+        apropos: yup
+            .string(),
+        facebook: yup
+            .string(),
+        linkedin: yup
+            .string(),
+        twitter: yup
+            .string()
+    }
+
+    const validationSchema = yup.object().shape(formFieldsRecruteur);
+
+    const formik = useFormik({
+        initialValues: formRecruteur,
+        validationSchema: validationSchema,
+        enableReinitialize: true,
+        onSubmit: () => {
+            handleUpdateRecruteur()
+        }
+    })
+
+    useEffect(() => {
+        if (recruteur_infos) {
+            setFormRecruteur({
+                user_name: recruteur_infos?.user_name,
+                user_email: recruteur_infos?.user_email,
+                user_role: recruteur_infos?.user_role,
+                site_web: recruteur_infos?.site_web,
+                siege_social: recruteur_infos?.siege_social,
+                effectif: recruteur_infos?.effectif,
+                secteur: recruteur_infos?.secteur,
+                apropos:recruteur_infos?.apropos,
+                reseaux_sociaux: {
+                    facebook: recruteur_infos?.reseaux_sociaux?.facebook,
+                    linkedin: recruteur_infos?.reseaux_sociaux?.linkedin,
+                    twitter: recruteur_infos?.reseaux_sociaux?.twitter,
+                }
+            })
+        }
+    }, [recruteur_infos])
+
+    useEffect(() => {
+        if (users?.user_id) {
+            dispatch(getInfosRecruteur(token, users?.user_id))
+        }
+    }, [users])
+
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>DATA",recruteur_infos);
+    
+
+    useEffect(() => {
+        setFormRecruteur({
+            ...formRecruteur,
+            reseaux_sociaux: reseauxList
+        })
+    }, [reseauxList])
+
+    const handleTextChange = (value: any, field:string) => {
+        if (field == 'facebook' || field == "linkedin" || field == "twitter"){
+            setReseauxList({
+                ...reseauxList,
+                [field] : value,
+            })
+        }else{
+            setFormRecruteur({
+                ...formRecruteur,
+                [field]: value
+            })
+        }
+    }
+
+    console.log("!!!!!!!!!!!!!!!!!!!!!!", formRecruteur);
+    
 
     const handleImageClick = () => {
         inputRef?.current?.click();
     };
 
-    const handleImageChange = (e:any) => {
+    const handleImageChange = (e: any) => {
         const file = e.target.files[0]
         setImage(file)
+    }
+
+    const handleUpdateRecruteur = () => {
+        const { user_name, user_email, site_web, siege_social, effectif, secteur, apropos, reseaux_sociaux } = formRecruteur
+        const { facebook, linkedin, twitter } = reseaux_sociaux
+        const dataInfo = {
+            recruteurData: {
+                site_web,
+                siege_social,
+                effectif,
+                secteur,
+                apropos,
+                reseaux_sociaux: {
+                    facebook,
+                    linkedin,
+                    twitter
+                }
+            },
+            userData: {
+                name: user_name,
+                email: user_email,
+            }
+        }
+
+        dispatch(updateRecruteur(dataInfo, users?.user_id))
     }
 
     return (
@@ -27,22 +184,22 @@ const Profil = () => {
                 Profil
             </div>
             <div className=' mt-8 flex justify-between w-full space-x-5'>
-                <form className="w-full space-y-7" >
+                <form className="w-full space-y-7" onSubmit={formik.handleSubmit}>
                     <div className="flex justify-between w-full border p-6 items-center">
                         <div className="w-1/3" onClick={handleImageClick}>
                             {
-                                image ? <img src={URL.createObjectURL(image)} className="w-40"  /> : <img src={profil} className="w-40"  />
+                                image ? <img src={URL.createObjectURL(image)} className="w-40" /> : <img src={profil} className="w-40" />
                             }
-                            <input type="file" ref={inputRef} onChange={handleImageChange} className="hidden"/>
+                            <input type="file" ref={inputRef} onChange={handleImageChange} className="hidden" />
                         </div>
                         <div className="w-2/3">
                             <TextField
                                 fullWidth
-                                label=""
-                                id=""
-                            // value={formik.values.email || ''}
-                            // onChange={(e) => handleTextChange(e?.target?.value, 'site-web')}
-                            // error={formik.touched.email && Boolean(formik.errors.email)}
+                                label="Nom societe"
+                                id="name_societe"
+                                value={formik.values.user_name || ''}
+                                onChange={(e) => handleTextChange(e?.target?.value, 'user_name')}
+                                error={formik.touched.user_name && Boolean(formik.errors.user_name)}
                             />
                         </div>
                     </div>
@@ -53,9 +210,9 @@ const Profil = () => {
                                 fullWidth
                                 label="Site web"
                                 id="site-web"
-                            // value={formik.values.email || ''}
-                            // onChange={(e) => handleTextChange(e?.target?.value, 'site-web')}
-                            // error={formik.touched.email && Boolean(formik.errors.email)}
+                                value={formik.values.site_web || ''}
+                                onChange={(e) => handleTextChange(e?.target?.value, 'site_web')}
+                                error={formik.touched.site_web && Boolean(formik.errors.site_web)}
                             />
                         </div>
                         <div className="w-full">
@@ -64,9 +221,9 @@ const Profil = () => {
                                 fullWidth
                                 label="Siège social"
                                 id="siege-social"
-                            // value={formik.values.phone || ''}
-                            // onChange={(e) => handleTextChange(e?.target?.value, 'siege-social')}
-                            // error={formik.touched.phone && Boolean(formik.errors.phone)}
+                                value={formik.values.siege_social || ''}
+                            onChange={(e) => handleTextChange(e?.target?.value, 'siege_social')}
+                            error={formik.touched.siege_social && Boolean(formik.errors.siege_social)}
                             />
                         </div>
                     </div>
@@ -77,9 +234,9 @@ const Profil = () => {
                                 fullWidth
                                 label="Effectif"
                                 id="effectif"
-                            // value={formik.values.email || ''}
-                            // onChange={(e) => handleTextChange(e?.target?.value, 'effectif')}
-                            // error={formik.touched.email && Boolean(formik.errors.email)}
+                            value={formik.values.effectif || ''}
+                            onChange={(e) => handleTextChange(e?.target?.value, 'effectif')}
+                            error={formik.touched.effectif && Boolean(formik.errors.effectif)}
                             />
                         </div>
                         <div className="w-full">
@@ -88,9 +245,9 @@ const Profil = () => {
                                 fullWidth
                                 label="Secteur"
                                 id="secteur"
-                            // value={formik.values.phone || ''}
-                            // onChange={(e) => handleTextChange(e?.target?.value, 'secteur')}
-                            // error={formik.touched.phone && Boolean(formik.errors.phone)}
+                            value={formik.values.secteur || ''}
+                            onChange={(e) => handleTextChange(e?.target?.value, 'secteur')}
+                            error={formik.touched.secteur && Boolean(formik.errors.secteur)}
                             />
                         </div>
                     </div>
@@ -103,6 +260,9 @@ const Profil = () => {
                             multiline
                             rows={10}
                             variant="outlined"
+                            value={formik.values.apropos || ''}
+                            onChange={(e) => handleTextChange(e?.target?.value, 'apropos')}
+                            error={formik.touched.apropos && Boolean(formik.errors.apropos)}
                         />
                     </div>
                     <div className="w-full">
@@ -120,6 +280,9 @@ const Profil = () => {
                                     ),
                                 }}
                                 variant="outlined"
+                                value={formik.values.reseaux_sociaux?.facebook || ''}
+                                onChange={(e) => handleTextChange(e?.target?.value, 'facebook')}
+                                error={formik.touched.reseaux_sociaux?.facebook && Boolean(formik.errors.reseaux_sociaux?.facebook)}
                             />
                             <TextField
                                 fullWidth
@@ -133,6 +296,9 @@ const Profil = () => {
                                     ),
                                 }}
                                 variant="outlined"
+                                value={formik.values.reseaux_sociaux?.linkedin || ''}
+                                onChange={(e) => handleTextChange(e?.target?.value, 'linkedin')}
+                                error={formik.touched.reseaux_sociaux?.linkedin && Boolean(formik.errors.reseaux_sociaux?.linkedin)}
                             />
                             <TextField
                                 fullWidth
@@ -146,6 +312,9 @@ const Profil = () => {
                                     ),
                                 }}
                                 variant="outlined"
+                                value={formik.values.reseaux_sociaux?.twitter || ''}
+                                onChange={(e) => handleTextChange(e?.target?.value, 'twitter')}
+                                error={formik.touched.reseaux_sociaux?.twitter && Boolean(formik.errors.reseaux_sociaux?.twitter)}
                             />
                         </div>
                     </div>
